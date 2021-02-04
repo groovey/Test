7d04d547ad4cc8235bfda417ed0f745c0abc1a5d
groovey <pokoot@gmail.com>
2021-02-04 23:15:08 +0800

    More changes on the checker

--- checker.php
+++ checker.php
@@ -6,7 +6,12 @@ use App\Services\DB;
  
 // Connect to postgres datababase
 $db = (new DB())->getConnection();
- 
-print "<br/> Connection DB Resource: ";
+
+print "<br/> DB host = " . getenv('DB_HOST');
+print "<br/> DB username = " . getenv('DB_USERNAME');
+
+print "<br/><br/> Connection DB Resource: ";
 
 var_dump($db);
+
+exit();
